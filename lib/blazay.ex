53623867867b9bde94b7b9a5e11da589ca436716
Zack Siri <zack@codemy.net>
2017-06-01 18:03:45 +0700

    initial commit

---
+++ lib/blazay.ex
@@ -0,0 +1,18 @@
+defmodule Blazay do
+  @moduledoc """
+  Documentation for Blazay.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Blazay.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/blazay_test.exs
@@ -0,0 +1,8 @@
+defmodule BlazayTest do
+  use ExUnit.Case
+  doctest Blazay
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
