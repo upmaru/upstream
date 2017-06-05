44cca9b1113c6ec163498b417bcbdc28bacce180
Zack Siri <zack@codemy.net>
2017-06-05 14:34:52 +0700

    added error output

--- lib/blazay/account.ex
+++ lib/blazay/account.ex
@@ -1,6 +1,6 @@
 defmodule Blazay.Account do
   import HTTPoison, only: [get: 3]
-  alias Blazay.Response.Authorization
+  alias Blazay.Response.{Authorization, Error}
 
   alias Blazay.Url
 
@@ -13,13 +13,15 @@ defmodule Blazay.Account do
     application_key: <whatever application_key>
   """
 
-  @spec authorize :: %Authorization{}
+  @spec authorize :: %Authorization{} | %Error{}
   def authorize do
     url = Url.generate(:authorize_account)
 
     case get(url, authorization_header(), params: []) do
-      {:ok, %{status_code: 200, body: body}} -> deserialize(body)
-      {:error, error} -> error
+      {:ok, %{status_code: 200, body: body}} ->
+        deserialize(:success, body)
+      {:ok, %{status_code: _, body: body}} ->
+        deserialize(:error, body)
     end
   end
 
@@ -31,7 +33,17 @@ defmodule Blazay.Account do
     [{"Authorization", encoded}]
   end
 
-  defp deserialize(body) do
+  defp deserialize(:error, body) do
+    {:ok, response} = Poison.decode(body)
+
+    %Error{
+      status: response["status"],
+      code: response["code"],
+      message: response["message"]
+    }
+  end
+
+  defp deserialize(:success, body) do
     {:ok, response} = Poison.decode(body)
 
     %Authorization{
