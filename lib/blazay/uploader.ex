5e6a35cd02c827e6b4f7cd37a0ef5796c8471333
Zack Siri <zack@codemy.net>
2017-06-20 22:49:06 +0700

    clean up api for uploader

--- lib/blazay/uploader.ex
+++ lib/blazay/uploader.ex
@@ -1,7 +1,15 @@
 defmodule Blazay.Uploader do
-  alias Blazay.Uploader.Supervisor
+  alias Blazay.Uploader.{
+    Supervisor, 
+    LargeFile
+  }
 
-  def start(:large_file, file_path) do
+  def start(file_path, :large_file) do
     Supervisor.start_large_file(file_path)
   end
+
+  def entry(pid, :large_file),  do: pid |> LargeFile.get(:entry)
+  def job(pid, :large_file),    do: pid |> LargeFile.get(:job)
+  def cancel(pid, :large_file), do: pid |> LargeFile.cancel
+  def upload(pid, :large_file), do: pid |> Largefile.upload
 end
