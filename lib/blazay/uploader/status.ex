a291c14051a00ac5782274262ae0d39137f21620
Zack Siri <zack@codemy.net>
2017-07-06 17:26:49 +0700

    need status to be able to handle sha1 in the correct order
    Former-commit-id: f2c662d695d5c72211a4edb711bfb55ed156f7f0

--- lib/blazay/uploader/status.ex
+++ lib/blazay/uploader/status.ex
@@ -2,16 +2,28 @@ defmodule Blazay.Uploader.Status do
   @moduledoc """
   used to track the status of the upload process
   """
+  defstruct [:sha1_array, :progress]
 
+  @type t :: %__MODULE__{
+    sha1_array: map,
+    progress: map
+  }
+
+  # TODO need to track sha1 array order correctly
   def start_link do
-    Agent.start_link(fn -> %{} end)
+    Agent.start_link(fn ->
+      %__MODULE__{
+        sha1_array: %{},
+        progress: %{}
+      } 
+    end)
   end
 
   def stop(pid), do: Agent.stop(pid)
 
   def get(pid) do
     Agent.get pid, fn reports ->
-      reports
+      reports.progress
       |> Enum.map(fn {_, status} -> (status || 0) end)
       |> Enum.sum
       |> Float.round(2)
