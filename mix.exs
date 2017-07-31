a0d5683571efcec4e19d33d044eb9582a002be16
Zack Siri <zack@codemy.net>
2017-07-31 10:59:10 +0700

    update version and elixir platform version
    Former-commit-id: cb1a34f3543edb7f71ffe3e8a0fb50fffe44505d

--- mix.exs
+++ mix.exs
@@ -1,11 +1,14 @@
 defmodule Blazay.Mixfile do
+  @moduledoc """
+  Mixfile for project.
+  """
   use Mix.Project
 
   def project do
     [
       app: :blazay,
-      version: "0.1.0",
-      elixir: "~> 1.4",
+      version: "1.0.0",
+      elixir: "~> 1.5",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       source_url: "https://github.com/artellectual/blazay",
