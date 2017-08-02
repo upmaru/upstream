665a4ff8ee43ea48e5ef96a9cddd7ec307321836
Zack Siri <zack@codemy.net>
2017-08-02 17:24:04 +0700

    update version

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Blazay.Mixfile do
   def project do
     [
       app: :blazay,
-      version: "1.0.4",
+      version: "1.0.5",
       elixir: "~> 1.5",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
