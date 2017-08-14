141d62fa76a9980cbbde2a860165d9ef8d1b395a
Zack Siri <zack@codemy.net>
2017-08-14 12:34:09 +0700

    Added ability to generate download url
    Former-commit-id: f5b83dbf3e0fcd7fa171abb4caab915c64cac243

--- config/dev.exs
+++ config/dev.exs
@@ -4,4 +4,5 @@ config :blazay, Blazay,
   account_id: System.get_env("B2_ACCOUNT_ID"),
   application_key: System.get_env("B2_APPLICATION_KEY"),
   bucket_id: System.get_env("B2_BUCKET_ID"),
+  bucket_name: System.get_env("B2_BUCKET_NAME"),
   concurrency: 2
