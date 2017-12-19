b5cf8823554108baf116b475b60d44e6eb4f38fe
Zack Siri <zack@codemy.net>
2017-12-19 13:26:17 +0700

    renamed CDN endpoint and added a download flavor
    Former-commit-id: 3e02f8aa7527c946060bf4a95ebbc40c65329996

---
+++ lib/blazay/downstream.ex
@@ -0,0 +1,36 @@
+defmodule Blazay.Downstream do
+  import Blazay.Endpoint
+  use Plug.Router
+
+  alias Blazay.B2
+
+  plug :match
+  plug :dispatch
+
+  get "/vod/:prefix/*path" do
+    render_json(conn, 200, %{
+      sequences: [%{
+        clips: [%{
+          type: "source", 
+          path: get_location(prefix, path)
+        }]
+      }]
+    })
+  end
+
+  get "/download/:prefix/*path" do
+    render_json(conn, 200, %{
+      data: %{ url: get_location(prefix, path) }
+    })
+  end
+
+  defp get_location(prefix, path) do
+    {:ok, %{authorization_token: token}} =
+      B2.Download.authorize(prefix, 3600)
+
+    ["/" <> Blazay.config(:bucket_name), 
+      prefix, path, token]
+    |> List.flatten
+    |> Enum.join("/")
+  end
+end
