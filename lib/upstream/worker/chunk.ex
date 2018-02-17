552f14d49c57c63bf0a2f9011ae735fcd92f2448
Zack Siri <zack@codemy.net>
2018-02-17 23:46:19 +0700

    support resumable upload
    Former-commit-id: eded1edb9dc757bffa82116dd0fd158e073cb3f7

--- lib/upstream/worker/chunk.ex
+++ lib/upstream/worker/chunk.ex
@@ -5,24 +5,27 @@ defmodule Upstream.Worker.Chunk do
   use Upstream.Worker.Base
 
   def task(state) do
-    {:ok, checksum} = Checksum.start_link()
-    {:ok, part_url} = Upload.part_url(state.uid.file_id)
+    with {:ok, checksum} <- Checksum.start_link(),
+         {:ok, part_url} <- Upload.part_url(state.uid.file_id) do
 
-    index = state.uid.index
+      index = state.uid.index
 
-    header = %{
-      authorization: part_url.authorization_token,
-      x_bz_part_number: index + 1,
-      content_length: state.job.content_length + 40,
-      x_bz_content_sha1: "hex_digits_at_end"
-    }
+      header = %{
+        authorization: part_url.authorization_token,
+        x_bz_part_number: index + 1,
+        content_length: state.job.content_length + 40,
+        x_bz_content_sha1: "hex_digits_at_end"
+      }
 
-    body = Flow.generate(state.job.stream, index, checksum)
+      body = Flow.generate(state.job.stream, index, checksum)
 
-    try do
-      Upload.part(part_url.upload_url, header, body)
-    after
-      Checksum.stop(checksum)
+      try do
+        Upload.part(part_url.upload_url, header, body)
+      after
+        Checksum.stop(checksum)
+      end
+    else
+      {:error, reason} -> {:error, reason}
     end
   end
 end
