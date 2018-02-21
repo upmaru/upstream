72ddc4422de8f2fc2aee568685935e0d2591920e
Zack Siri <zack@codemy.net>
2018-02-21 14:23:14 +0700

    added ability to list files by name

---
+++ lib/upstream/b2/list/file_names.ex
@@ -0,0 +1,18 @@
+defmodule Upstream.B2.List.FileNames do
+  @moduledoc """
+  Lists files by file name
+  """
+
+  defstruct [:files, :next_file_name]
+
+  use Upstream.B2.Base
+
+  def url(_), do: Url.generate(Account.api_url(), :list_file_names)
+
+  def body(file_name) when is_binary(file_name) do
+    %{
+      bucketId: Upstream.config(:bucket_id),
+      startFileName: URI.encode(file_name)
+    }
+  end
+end
