72c2e0dfda8c40143f272febb5730f8f22bdd308
Zack Siri <zack@codemy.net>
2018-05-03 14:38:09 +0700

    normalized ets / redis store better
    Former-commit-id: e104fd8b12929d477e1e803e625ead267b2b3734

--- test/store/store_test.exs
+++ test/store/store_test.exs
@@ -5,40 +5,38 @@ defmodule Upstream.StoreTest do
   use ExUnit.Case
   alias Upstream.Store
 
-  describe "ets" do
-    test "store value in key" do
-      Store.set("blah", "test")
+  test "store value in key" do
+    Store.set("blah", "test")
 
-      assert Store.get("blah") == "test"
-    end
+    assert Store.get("blah") == "test"
+  end
 
-    test "add member to list" do
-      Store.add_member("uploading", "some_job")
+  test "add member to list" do
+    Store.add_member("uploading", "some_job")
 
-      assert Store.get("uploading") == ["some_job"]
-    end
+    assert Store.get("uploading") == ["some_job"]
+  end
 
-    test "move member from 1 list to another" do
-      Store.add_member("uploading", "some_job")
-      Store.add_member("errored", "another_job")
-      Store.move_member("uploading", "errored", "some_job")
+  test "move member from 1 list to another" do
+    Store.add_member("uploading", "some_job")
+    Store.add_member("errored", "another_job")
+    Store.move_member("uploading", "errored", "some_job")
 
-      assert Store.get("errored") == ["some_job", "another_job"]
-    end
+    assert Store.get("errored") == ["some_job", "another_job"]
+  end
 
-    test "remove item from list" do
-      Store.add_member("uploading", "some_job")
+  test "remove item from list" do
+    Store.add_member("uploading", "some_job")
 
-      Store.remove_member("uploading", "some_job")
+    Store.remove_member("uploading", "some_job")
 
-      assert Store.get("uploading") == []
-    end
+    assert Store.get("uploading") == nil
+  end
 
-    test "store and retrieve hash" do
-      example_map = %{"blah" => "example", "another" => "test"}
-      Store.set("some_hash", example_map)
+  test "store and retrieve hash" do
+    example_map = %{"blah" => "example", "another" => "test"}
+    Store.set("some_hash", example_map)
 
-      assert Store.get("some_hash") == example_map
-    end
+    assert Store.get("some_hash") == example_map
   end
 end
