9d2462c8ce807646c7ae2c9350c9d1d490edaed4
Zack Siri <zack@codemy.net>
2018-06-06 14:12:05 +0700

    better error handling for worker
    Former-commit-id: 009b2d9acd28251206ecb6a283bf593b9a5a0476

--- lib/upstream/uploader/large_file.ex
+++ lib/upstream/uploader/large_file.ex
@@ -25,8 +25,14 @@ defmodule Upstream.Uploader.LargeFile do
          {:ok, result} <- Worker.LargeFile.upload(job.uid.name) do
       {:ok, result}
     else
-      {:error, {reason, _}} -> {:error, %{error: reason}}
-      {:error, reason} -> {:error, reason}
+      {:error, {:already_started, _}} -> {:error, %{error: :already_started}}
+      {:error, {reason, _}} ->
+        message = %{error: reason}
+        Job.error(job, message)
+        {:error, message}
+      {:error, reason} ->
+        Job.error(job, reason)
+        {:error, reason}
     end
   end
 end
