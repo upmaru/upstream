5e32999f3ce730b7c015dd2e7c9dbc813b12fa5f
Zack Siri <zack@codemy.net>
2018-06-06 14:00:23 +0700

    update version
    Former-commit-id: 1bd21a69196d1eeb4c15bb877bb6d5f8ffe92fd8

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Upstream.Mixfile do
   def project do
     [
       app: :upstream,
-      version: "1.6.0",
+      version: "1.6.1",
       elixir: "~> 1.6.0",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
