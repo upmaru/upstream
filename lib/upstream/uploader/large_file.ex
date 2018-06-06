e997a17a4813fb66dfd2e1d4c6fcc23411d01b06
Zack Siri <zack@codemy.net>
2018-06-06 12:10:29 +0700

    clean up error handling for registry when already started

--- lib/upstream/uploader/large_file.ex
+++ lib/upstream/uploader/large_file.ex
@@ -5,6 +5,7 @@ defmodule Upstream.Uploader.LargeFile do
   use Supervisor
 
   alias Upstream.Worker
+  alias Upstream.Job
 
   def start_link do
     Supervisor.start_link(__MODULE__, [], name: __MODULE__)
@@ -19,10 +20,12 @@ defmodule Upstream.Uploader.LargeFile do
   end
 
   def perform(job) do
-    with {:ok, _pid} <- Supervisor.start_child(__MODULE__, [job]),
+    with {:ok, _value} <- Job.start(job),
+         {:ok, _pid} <- Supervisor.start_child(__MODULE__, [job]),
          {:ok, result} <- Worker.LargeFile.upload(job.uid.name) do
       {:ok, result}
     else
+      {:error, {:already_started, _pid}} -> {:error, %{error: :already_started}}
       {:error, reason} -> {:error, reason}
     end
   end
