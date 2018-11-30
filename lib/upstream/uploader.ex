b389b7bfff61e3892ed7f494f1523b7d0e108c29
Zack Siri <zack@artellectual.com>
2018-11-30 20:53:26 +0700

    cleaned up uploader by using dynamic supervisor

--- lib/upstream/uploader.ex
+++ lib/upstream/uploader.ex
@@ -3,28 +3,42 @@ defmodule Upstream.Uploader do
   Manages Supervisors for Uploaders
   """
 
-  use Supervisor
+  use DynamicSupervisor
 
   alias Upstream.{
-    Job, Uploader, Worker
+    Job, Worker
   }
 
   @spec start_link(any()) :: :ignore | {:error, any()} | {:ok, pid()}
-  def start_link(_args) do
-    Supervisor.start_link(__MODULE__, [], name: __MODULE__)
+  def start_link([]) do
+    DynamicSupervisor.start_link(__MODULE__, [], name: __MODULE__)
   end
 
   @impl true
-  @spec init(any()) :: {:ok, {%{intensity: any(), period: any(), strategy: any()}, [any()]}}
+  @spec init(any()) ::
+          {:ok,
+           %{
+             extra_arguments: [any()],
+             intensity: non_neg_integer(),
+             max_children: :infinity | non_neg_integer(),
+             period: pos_integer(),
+             strategy: :one_for_one
+           }}
   def init(_) do
-    children = [
-      {__MODULE__.Chunk, []},
-      {__MODULE__.LargeFile, []},
-      {__MODULE__.StandardFile, []},
-      {Task.Supervisor, name: __MODULE__.TaskSupervisor}
-    ]
+    DynamicSupervisor.init(strategy: :one_for_one)
+  end
 
-    Supervisor.init(children, strategy: :one_for_one)
+  @spec start_worker(atom() | binary(), any()) ::
+          :ignore | {:error, any()} | {:ok, pid()} | {:ok, pid(), any()}
+  def start_worker(worker_module, job) do
+    module = Module.concat(Worker, worker_module)
+
+    child_spec = {module, job}
+    case DynamicSupervisor.start_child(__MODULE__, child_spec) do
+      {:ok, pid} -> {:ok, pid, module}
+      {:error, reason} -> {:error, reason}
+      :ignore -> :ignore
+    end
   end
 
   @spec upload_chunk!(binary(), binary() | %{file_id: any(), index: any()}) :: {:error, any()} | {:ok, any()}
@@ -32,9 +46,7 @@ defmodule Upstream.Uploader do
     job = Job.create(chunk_path, params)
     if Job.State.errored?(job), do: Job.State.retry(job)
 
-    start_and_register(job, fn ->
-      start_uploader(Chunk, job)
-    end)
+    start_and_register(job, fn -> start_upload(Chunk, job) end)
   end
 
   @spec upload_file!(binary(), binary() | %{file_id: any(), index: any()}, any()) :: {:error, any()} | {:ok, any()}
@@ -42,14 +54,12 @@ defmodule Upstream.Uploader do
     job = Job.create(file_path, name, metadata)
     if Job.State.errored?(job), do: Job.State.retry(job)
 
-    file_type =
+    worker_type =
       if job.threads == 1,
         do: StandardFile,
         else: LargeFile
 
-    start_and_register(job, fn ->
-      start_uploader(file_type, job)
-    end)
+    start_and_register(job, fn -> start_upload(worker_type, job) end)
   end
 
   defp start_and_register(job, on_start) do
@@ -71,9 +81,9 @@ defmodule Upstream.Uploader do
     end
   end
 
-  defp start_uploader(module, job) do
-    with {:ok, pid} <- DynamicSupervisor.start_link(Module.concat(Uploader, module), [job]),
-         {:ok, result} <- Module.concat(Worker, module).upload(pid)
+  defp start_upload(module, job) do
+    with {:ok, pid, module} <- start_worker(module, job),
+         {:ok, result} <- module.upload(pid)
     do
       {:ok, result}
     else
