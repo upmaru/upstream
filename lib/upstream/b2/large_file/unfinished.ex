e8ccc1757f7a8d74211257c4bbb8e3ace3f27adc
Zack Siri <zack@artellectual.com>
2018-12-17 17:24:59 +0700

    fix type for unfinished

--- lib/upstream/b2/large_file/unfinished.ex
+++ lib/upstream/b2/large_file/unfinished.ex
@@ -17,7 +17,7 @@ defmodule Upstream.B2.LargeFile.Unfinished do
 
   use Upstream.B2.Base
 
-  @spec url(atom() | %{api_url: any()}, any()) :: binary()
+  @spec url(Upstream.B2.Account.Authorization.t(), any()) :: binary()
   def url(auth, _), do: Url.generate(auth.api_url, :list_unfinished_large_files)
 
   def body(_) do
